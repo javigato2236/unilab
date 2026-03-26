e8f75c6e8a53582c55a91ec83e5b5f6e34215d3f
javier sanchez <javieraugustosanchezmartinez@gmail.com>
2026-03-26 10:56:00 -0500

    se configuro parte de la seleccion de pictogramas de ingreso de reactivos

--- frontend/src/components/tablaInventario.jsx
+++ frontend/src/components/tablaInventario.jsx
@@ -1,7 +1,19 @@
 import iconoAgregarRegistro from '../assets/iconoAgregarRegistro.png';
 import '../styles/paginaPrincipal.css';
+import '../styles/pictogramas.css';
 import Modal from '../hoosk/modalReutilizable';
 import { useState } from 'react';
+import advertencia from '../assets/advertencia.png';
+import carcinogenico from '../assets/carcinogenico.png';
+import corrosivo from '../assets/corrosivo.png';
+import explosivo from '../assets/explosivo.png';
+import gases from '../assets/gases.png';
+import inflamable from '../assets/inflamable.png';
+import medioAmbiente from '../assets/medioAmbiente.png';
+import oxidante from '../assets/oxidante.png';
+import toxico from '../assets/toxico.png';
+
+
 
 function TablaReactivos({ reactivos, seleccionarReactivo }) {
   const [isModalOpen, setIsModalOpen] = useState(false);
@@ -24,6 +36,19 @@ function TablaReactivos({ reactivos, seleccionarReactivo }) {
   const [isThirdModalOpen, setIsThirdOpen] = useState(false);
   const openThirdModal = () => setIsThirdOpen(true);
   const closeThirdModal = () => setIsThirdOpen(false);
+
+   const [isPictogramasdModalOpen, setIsPictogramasOpen] = useState(false);
+  const openPictogramasModal = () => setIsPictogramasOpen(true);
+  const closePictogramasModal = () => setIsPictogramasOpen(false);
+
+  const [pictogramasSeleccionados, setPictogramasSeleccionados] = useState([]);
+  const togglePictograma = (nombre) => {
+  setPictogramasSeleccionados((prev) =>
+    prev.includes(nombre)
+      ? prev.filter((p) => p !== nombre)
+      : [...prev, nombre]
+  );
+};
 
    
 
@@ -67,6 +92,9 @@ function TablaReactivos({ reactivos, seleccionarReactivo }) {
       <Modal isOpen={isFirstModalOpen} onClose={closeFirstModal}>
         <form >
 
+          <label htmlFor="">Familia</label>
+          <input type="text" />
+
           <label htmlFor="">Nombre</label>
           <input type="text" />
 
@@ -85,18 +113,84 @@ function TablaReactivos({ reactivos, seleccionarReactivo }) {
           <label htmlFor="">Estado</label>
           <input type="text" />
 
+          <button type="button" onClick={openPictogramasModal}>Pictogramas de seguridad</button>
+
         </form>
+        
+
         <button>Enviar</button>
       </Modal>
 
+
+
       <Modal isOpen={isSecondModalOpen} onClose={closeSecondModal}>
-        <h1>modal 2</h1>
+        <form>
+
+          <label htmlFor="">Cantidad</label>
+          <input type="text" />
+
+          <label htmlFor="">Presentacion</label>
+          <input type="text" />
+
+          <label htmlFor="">Unidad de medida</label>
+          <input type="text" />
+
+          <label htmlFor="">Numero de recipientes</label>
+          <input type="text" />
+
+           <label htmlFor="">Catidad total</label>
+          <input type="text" />
+
+          <label htmlFor="">Cantidad real</label>
+          <input type="text" />
+
+          <label htmlFor="">Hubicacion especifica</label>
+          <input type="text" />
+      
+        </form>
         <button>Enviar</button>
       </Modal>
 
       <Modal isOpen={isThirdModalOpen} onClose={closeThirdModal}>
         <h1>modal 3</h1>
         <button>Enviar</button>
+      </Modal>
+
+      <Modal isOpen={isPictogramasdModalOpen} onClose={closePictogramasModal}>
+        <div className="contenedor-pictogramas">
+
+              {[
+                { nombre: "advertencia", img: advertencia },
+                { nombre: "carcinogenico", img: carcinogenico },
+                { nombre: "corrosivo", img: corrosivo },
+                { nombre: "explosivo", img: explosivo },
+                { nombre: "gases", img: gases },
+                { nombre: "inflamable", img: inflamable },
+                { nombre: "medioAmbiente", img: medioAmbiente },
+                { nombre: "oxidante", img: oxidante },
+                { nombre: "toxico", img: toxico },
+              ].map((p) => (
+                <label key={p.nombre} className={`pictograma-item 
+                  ${pictogramasSeleccionados.includes(p.nombre) ? "seleccionado" : ""}`}>
+
+                  <input
+                    type="checkbox"
+                    checked={pictogramasSeleccionados.includes(p.nombre)}
+                    onChange={() => togglePictograma(p.nombre)}
+                  />
+
+                  <img src={p.img} alt={p.nombre} />
+
+                </label>
+              ))}
+
+            </div>
+        <button onClick={() => {
+    console.log(pictogramasSeleccionados);
+    closePictogramasModal();
+  }}>
+    Guardar
+  </button>
       </Modal>
 
      
